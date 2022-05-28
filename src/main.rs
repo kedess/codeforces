3edc07c3e5ba45ed62025d062090916f40d46ee9
kedess <amazing-hash@yandex.ru>
2022-05-28 22:53:35 +0400

    Base implementation

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+#[allow(dead_code)]
+fn gets() -> String {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    s.trim_end().to_string()
+}
+
+#[allow(unused_macros)]
+macro_rules! read_vec {
+    ($ty:ty) => {{
+        let line = gets();
+        let mut it = line.split_whitespace();
+        let mut v = Vec::new();
+        while let Some(value) = it.next() {
+            v.push(value.parse::<$ty>().unwrap());
+        }
+        v
+    }}
+}
+
+#[allow(unused_macros)]
+macro_rules! read {
+    ( $ty:ty) => {
+        gets().split_whitespace().next().unwrap().parse::<$ty>().unwrap()
+    };
+    ( $($ty:ty),* ) => {{
+        let line = gets();
+        let mut it = line.split_whitespace();
+        ( $(it.next().unwrap().parse::<$ty>().unwrap(),)* )
+    }};
+    ( $( $ty:ty ),+ ,) => {
+		read![ $( $ty ),* ]
+    };
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    writeln!(out, "{}", "Here is my answer").unwrap();
+}
